3e78ac51943903438a4360d8a75b9260029c7e11
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-16 11:03:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Welcome to Dhruv's Biology Calculator")
-chem=str(input('Enter the calculation(no capital letters(single word)): '))
+bio=str(input('Enter the calculation(no capital letters(single word)): '))
 
-if(chem=='ph'):
+if(bio=='ph'):
     concentration=float(input('Enter the hydrogen concentration:'))
     ph=-log(concentration)
     print('%0.3f is the pH.' %(ph))
